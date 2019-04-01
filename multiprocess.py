bdc4a3f8eb85cd727a1c839451f91df6e031e27a
sean bradley <seanwasere@gmail.com>
2019-04-01 10:23:25 +0100

    raise processor count to 10 in multiprocess.py

--- multiprocess.py
+++ multiprocess.py
@@ -3,7 +3,7 @@ from multiprocessing import Process
 import client
 
 if __name__ == "__main__":
-    COUNT = 2
+    COUNT = 10
     PROCESSES = {}
     for x in range(COUNT):
         PROCESSES[x] = Process(target=client.run)
